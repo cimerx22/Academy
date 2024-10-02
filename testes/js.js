b96ae0a6c7d5d9d3f602583733c6f52eef048f43
wellyngton <wellyngtonmonteiro84@gmail.com>
2024-10-02 17:43:43 -0300

    update planos

--- testes/js.js
+++ testes/js.js
@@ -2,59 +2,18 @@ let lista = document.querySelectorAll(".nav-items-planos ul li");
 
 let imagemPlanos = document.querySelector(".img-plano img");
 
-let imgs = ["../src/imgs/planos/planos jogador 1.png"];
+let precos = [
+	[50, 30, 30, 10, 40],
+	[50123, 1530, 30, 10, 40],
+	[503, 350, 330, 110, 20],
+	[50, 30, 512330, 10, 30],
+];
 
-let constructorPlanos = function (
-	avaliacao,
-	planoIntermediario,
-	planoMensal,
-	planoSemestral,
-	planoAnual,
-	img
-) {
-	this.av = avaliacao;
-	this.planoInter = planoIntermediario;
-	this.planoMensal = planoMensal;
-	this.planoSemestral;
-	this.planoSemestral = planoSemestral;
-	this.planoAnual = planoAnual;
-	this.img = img;
-};
+let img = document.querySelector(".img-plano img");
 
-let precos = {
-	boxe: constructorPlanos(
-		"25",
-		"50",
-		"75",
-		"100",
-		"125",
-		"../src/imgs/planos/img 1.png"
-	),
-	crossfit: constructorPlanos(
-		"25",
-		"50",
-		"75",
-		"100",
-		"125",
-		"../src/imgs/planos/img 2.png"
-	),
-	academia: constructorPlanos(
-		"25",
-		"50",
-		"75",
-		"100",
-		"125",
-		"../src/imgs/planos/img 3.png"
-	),
-	danca: constructorPlanos(
-		"25",
-		"50",
-		"75",
-		"100",
-		"125",
-		"../src/imgs/planos/img 4.png"
-	),
-};
+let img_planos = document.querySelectorAll(".img-plano img");
+
+let valores_planos = document.querySelectorAll(".valores");
 
 lista.forEach(function (elemento) {
 	elemento.addEventListener("click", function () {
@@ -62,24 +21,48 @@ lista.forEach(function (elemento) {
 			elemento.classList.toggle("actives", false);
 		});
 
-		if (elemento.classList.toggle("actives", true)) {
-			console.log(elemento.dataset.nome);
+		img_planos.forEach(function (elemento) {
+			elemento.classList.toggle("img-active", false);
+		});
 
+		valores_planos.forEach(function (valores_elemento) {
+			valores_elemento.classList.toggle("span-active", false);
+		});
+
+		if (elemento.classList.toggle("actives", true)) {
 			switch (elemento.dataset.nome) {
 				case "musc":
-					imagemPlanos.src = "../src/imgs/planos/planos jogador 1.png";
+
+					img_planos[0].classList.toggle("img-active", true);
+					valores_planos.forEach(function (el, ind) {
+						el.innerText = precos[0][ind];
+					});
+
+
 					break;
 
 				case "cros":
-					imagemPlanos.src = "../src/imgs/planos/img 1.jpg";
+					img_planos[1].classList.toggle("img-active", true);
+					valores_planos.forEach(function (el, ind) {
+						el.innerText = precos[1][ind];
+						
+					});
 					break;
-			
+
 				case "boxe":
-					imagemPlanos.src = "../src/imgs/planos/img 2.jpg";
+					img_planos[2].classList.toggle("img-active", true);
+					valores_planos.forEach(function (el, ind) {
+						el.innerText = precos[2][ind];
+						
+					});
 					break;
 
 				case "dance":
-					imagemPlanos.src = "../src/imgs/planos/img 3.jpg";
+					img_planos[3].classList.toggle("img-active", true);
+					valores_planos.forEach(function (el, ind) {
+						el.innerText = precos[3][ind];
+						
+					});
 					break;
 
 				default:
@@ -87,6 +70,23 @@ lista.forEach(function (elemento) {
 
 					break;
 			}
+
+			// valores_planos.forEach(function(el){
+			// 	el.classList.toggle("span-active", true); 
+			// })
+			
 		}
+
+		
+
+		// img.addEventListener('animationend', function(){
+		// 	img.classList.toggle("fade-1");
+		// 	console.log("terminou");
+		// }, {once: true})
+
+		// img.addEventListener('animationend', function(){
+		// 	img.classList.toggle("fade");
+
+		// }, {once:true})
 	});
 });
