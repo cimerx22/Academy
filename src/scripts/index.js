669178c1440089d4eb756bb13d69642b7421c7fe
Wellyngton <wellyngtonmonteiro84@gmail.com>
2024-10-15 07:16:11 -0300

    att mobile, não mexer no código ainda

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,144 +1,155 @@
-"use strict"
+// "use strict"
 
-let lista = document.querySelectorAll(".nav-items-planos ul li");
-let click = false;
-let indexPlanos;
-let valores_planos = document.querySelectorAll(".valores");
-let img_planos = document.querySelectorAll(".img-plano img");
+// let lista = document.querySelectorAll(".nav-items-planos ul li");
+// let click = false;
+// let indexPlanos;
+// let valores_planos = document.querySelectorAll(".valores");
+// let img_planos = document.querySelectorAll(".img-plano img");
 
-let precos = [
-	[50, 30, 30, 10, 40],
-	[50123, 1530, 30, 10, 40],
-	[503, 350, 330, 110, 20],
-	[50, 30, 512330, 10, 30],
-];
+// let precos = [
+// 	[50, 30, 30, 10, 40],
+// 	[50123, 1530, 30, 10, 40],
+// 	[503, 350, 330, 110, 20],
+// 	[50, 30, 512330, 10, 30],
+// ];
 
-lista.forEach(function (elemento) {
+// lista.forEach(function (elemento) {
 
-	let valorPreco;
+// 	let valorPreco;
 
-	elemento.addEventListener("click", function () {
+// 	elemento.addEventListener("click", function () {
 
-		if (click) return;
+// 		if (click) return;
 
-		click = true;
+// 		click = true;
 
-		lista.forEach(function (elemento) {
-			elemento.classList.toggle("actives", false);
-		});
+// 		lista.forEach(function (elemento) {
+// 			elemento.classList.toggle("actives", false);
+// 		});
 
-		img_planos.forEach(function (elemento) {
-			elemento.classList.toggle("img-active", false);
-		});
+// 		img_planos.forEach(function (elemento) {
+// 			elemento.classList.toggle("img-active", false);
+// 		});
 
-		valores_planos.forEach(function (valores_elemento) {
-			valores_elemento.classList.add("vLeft");
-		});
+// 		valores_planos.forEach(function (valores_elemento) {
+// 			valores_elemento.classList.add("vLeft");
+// 		});
 
-		setTimeout(()=>{
-			if (elemento.classList.toggle("actives", true)) {
-				switch (elemento.dataset.nome) {
-					case "musc":
+// 		setTimeout(()=>{
+// 			if (elemento.classList.toggle("actives", true)) {
+// 				switch (elemento.dataset.nome) {
+// 					case "musc":
 	
-						img_planos[0].classList.toggle("img-active", true);
-						indexPlanos = 0;
-						break;
+// 						img_planos[0].classList.toggle("img-active", true);
+// 						indexPlanos = 0;
+// 						break;
 	
-					case "cros":
-						img_planos[1].classList.toggle("img-active", true);
-						indexPlanos = 1;
-						break;
+// 					case "cros":
+// 						img_planos[1].classList.toggle("img-active", true);
+// 						indexPlanos = 1;
+// 						break;
 	
-					case "boxe":
-						img_planos[2].classList.toggle("img-active", true);
-						indexPlanos = 2;
-						break;
+// 					case "boxe":
+// 						img_planos[2].classList.toggle("img-active", true);
+// 						indexPlanos = 2;
+// 						break;
 	
-					case "dance":
-						img_planos[3].classList.toggle("img-active", true);
-						indexPlanos = 3;
-						break;
+// 					case "dance":
+// 						img_planos[3].classList.toggle("img-active", true);
+// 						indexPlanos = 3;
+// 						break;
 	
-					default:
-						console.log("algum erro");
-						break;
-				}
+// 					default:
+// 						console.log("algum erro");
+// 						break;
+// 				}
 	
-				valores_planos.forEach((valoresEl, ind)=>{
-					valoresEl.classList.remove("vLeft");
-					valorPreco = precos[indexPlanos][ind];
+// 				valores_planos.forEach((valoresEl, ind)=>{
+// 					valoresEl.classList.remove("vLeft");
+// 					valorPreco = precos[indexPlanos][ind];
 
-					valoresEl.innerText = valorPreco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
-				}) 
+// 					valoresEl.innerText = valorPreco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+// 				}) 
 				
-			}
+// 			}
 
-			click = false;
+// 			click = false;
 
-		}, 700)
+// 		}, 700)
 
-	});
-});
-
-
-
-
-// Equipe
-
-
-let prev = document.querySelector(".icon-prev-next i:nth-child(1)")
-let next = document.querySelector(".icon-prev-next i:nth-child(2)")
-let scrollTreinador = document.querySelector(".treinadores-content")
-let itemTreinador = document.querySelectorAll(".treinadores-content .container-content .box")
-
-
-function valorTotal(arr, valorWidth){
-
-	let valor = 0;
-
-	arr.forEach(()=>{
-		valor += valorWidth;
-	})
-
-	return valor;
-
-}
-
-prev.addEventListener("click", ()=>{
-	scrollTreinador.scrollLeft -= 400;
-})
-
-next.addEventListener("click", ()=>{
-	scrollTreinador.scrollLeft += 400;
-})
+// 	});
+// });
 
 
 
 
-let feeds = document.querySelector(".feeds");
-let itemsFeed = document.querySelectorAll(".feed-item");
-let containerTreinador = document.querySelector(".treinadores-content .container-content")
-
-containerTreinador.style.width = `${valorTotal(itemTreinador, 200)}px`;
-feeds.style.width = `${valorTotal(itemsFeed, 245)}px`
+// // Equipe
 
 
+// let prev = document.querySelector(".icon-prev-next i:nth-child(1)")
+// let next = document.querySelector(".icon-prev-next i:nth-child(2)")
+// let scrollTreinador = document.querySelector(".treinadores-content")
+// let itemTreinador = document.querySelectorAll(".treinadores-content .container-content .box")
 
-// hover animação
 
-let box = document.querySelectorAll(".item-treinadores");
+// function valorTotal(arr, valorWidth){
 
+// 	let valor = 0;
 
-// box.forEach((elemento)=>{
-// 	elemento.addEventListener("mouseenter",(tar)=>{
-// 		// tar.target.children[2].style.cssText = `bottom: 0;`;
-// 		tar.target.children[2].classList.toggle("hover-animation");
+// 	arr.forEach(()=>{
+// 		valor += valorWidth;
 // 	})
-// 	elemento.addEventListener("mouseleave", (tar)=>{
-// 		// tar.target.children[2].style.cssText = `/* bottom: 0px; */`;
-// 		tar.target.children[2].classList.toggle("hover-animation");
-// 	})
+
+// 	return valor;
+
+// }
+
+// prev.addEventListener("click", ()=>{
+// 	scrollTreinador.scrollLeft -= 400;
+// })
+
+// next.addEventListener("click", ()=>{
+// 	scrollTreinador.scrollLeft += 400;
 // })
 
 
+
+
+// let feeds = document.querySelector(".feeds");
+// let itemsFeed = document.querySelectorAll(".feed-item");
+// let containerTreinador = document.querySelector(".treinadores-content .container-content")
+
+// containerTreinador.style.width = `${valorTotal(itemTreinador, 200)}px`;
+// feeds.style.width = `${valorTotal(itemsFeed, 245)}px`
+
+
+
+// button menu animação
+
+
+let menu = document.querySelector(".button-menu");
+
+console.log(menu);
+
+menu.addEventListener("click", ()=>{
+
+	menu.classList.toggle("active-button")
+
+	let  controleMenu = menu.classList.contains("active-button");
+
+	if(controleMenu){
+		
+	}else{
+		
+	}
+
+
+	
+
+	
+
+
+	
+
+})
 
