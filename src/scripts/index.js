95e1d2e4ea914f56d79a7137bde52586f7f13804
Wellyngton <wellyngtonmonteiro84@gmail.com>
2024-10-19 12:55:39 -0300

    finalizando o mobile

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 let lista = document.querySelectorAll(".nav-items-planos ul li");
 let click = false;
@@ -14,11 +14,9 @@ let precos = [
 ];
 
 lista.forEach(function (elemento) {
-
 	let valorPreco;
 
 	elemento.addEventListener("click", function () {
-
 		if (click) return;
 
 		click = true;
@@ -35,11 +33,10 @@ lista.forEach(function (elemento) {
 			valores_elemento.classList.add("vLeft");
 		});
 
-		setTimeout(()=>{
+		setTimeout(() => {
 			if (elemento.classList.toggle("actives", true)) {
 				switch (elemento.dataset.nome) {
 					case "musc":
-
 						img_planos[0].classList.toggle("img-active", true);
 						indexPlanos = 0;
 						break;
@@ -60,95 +57,91 @@ lista.forEach(function (elemento) {
 						break;
 
 					default:
-						
 						break;
 				}
 
-				valores_planos.forEach((valoresEl, ind)=>{
+				valores_planos.forEach((valoresEl, ind) => {
 					valoresEl.classList.remove("vLeft");
 					valorPreco = precos[indexPlanos][ind];
 
-					valoresEl.innerText = valorPreco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
-				})
-
+					valoresEl.innerText = valorPreco.toLocaleString("pt-BR", {
+						style: "currency",
+						currency: "BRL",
+					});
+				});
 			}
 
 			click = false;
-
-		}, 700)
-
+		}, 700);
 	});
 });
 
 // Equipe
 
-let prev = document.querySelector(".icon-prev-next i:nth-child(1)")
-let next = document.querySelector(".icon-prev-next i:nth-child(2)")
-let scrollTreinador = document.querySelector(".treinadores-content")
-let itemTreinador = document.querySelectorAll(".treinadores-content .container-content .box")
+let prev = document.querySelector(".icon-prev-next i:nth-child(1)");
+let next = document.querySelector(".icon-prev-next i:nth-child(2)");
+let scrollTreinador = document.querySelector(".treinadores-content");
+let itemTreinador = document.querySelectorAll(
+	".treinadores-content .container-content .box"
+);
 
-function valorTotal(arr){
-
-	let valor = 0;
-	
-	arr.forEach((el)=>{
-		console.log((getComputedStyle(el).width));
-		valor += Number(getComputedStyle(el).width);
-	})
-
-	return valor;
-
-}
-
-prev.addEventListener("click", ()=>{
+prev.addEventListener("click", () => {
 	scrollTreinador.scrollLeft -= 400;
-})
+});
 
-next.addEventListener("click", ()=>{
+next.addEventListener("click", () => {
 	scrollTreinador.scrollLeft += 400;
-})
+});
 
 let feeds = document.querySelector(".feeds");
 let itemsFeed = document.querySelectorAll(".feed-item");
-let containerTreinador = document.querySelector(".treinadores-content .container-content")
+let containerTreinador = document.querySelector(
+	".treinadores-content .container-content"
+);
 
+let body = document.querySelector("body");
 
+// body.onload = function () {
+// 	// console.log("carregado");
+// 	// console.log(getComputedStyle(itemsFeed[1]).width);
 
-let body = document.querySelector("body")
+// };
 
-body.onload = function(){
-	console.log("carregado");
-	console.log(getComputedStyle(itemsFeed[0]).width);
-	containerTreinador.style.width = `${valorTotal(itemTreinador)}px`;
-	feeds.style.width = `${valorTotal(itemsFeed)}px`
+// document.addEventListener("DOMContentLoaded", ()=>{
+// 	console.log(getComputedStyle(itemsFeed[0]).width);
+// })
+
+if (body.scrollWidth > 451) {
+	containerTreinador.style.width = `${valorTotal(itemTreinador, 260)}px`;
+	feeds.style.width = `${valorTotal(itemsFeed, 214.48 + 30)}px`;
+}else if(body.scrollWidth < 451){
+	containerTreinador.style.width = `${valorTotal(itemTreinador, 145.01 + 50)}px`;
+	feeds.style.width = `${valorTotal(itemsFeed, 150 + 40)}px`;
 }
 
+function valorTotal(arr, width) {
+	let valor = 0;
 
+	arr.forEach(() => {
+		valor += width;
+	});
 
+	return valor;
+}
 
 // button menu animação
 
 let menu = document.querySelectorAll(".button-menu");
 let menuMobile = document.querySelector(".menu-mobile");
 
-
-
 menu.forEach((el) => {
-	
 	el.addEventListener("click", () => {
-
-		
-		
-		
 		el.classList.toggle("active-button");
-
 
 		let controleMenu = el.classList.contains("active-button");
 
-		
 		if (controleMenu) {
-			menuMobile.style.transform = "translateX(0)";	
-			
+			menuMobile.style.transform = "translateX(0)";
 		} else {
 			menuMobile.style.transform = "translateX(100%)";
 		}
